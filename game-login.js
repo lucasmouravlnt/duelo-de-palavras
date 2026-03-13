981bf493cbf013fde5cbce85e3bb1c680163734e
Lucas Moura <lucasmouravlnt@gmail.com>
2026-03-12 21:58:30 -0300

    função addUser  e estilização do index

---
+++ game-login.js
@@ -0,0 +1,8 @@
+function addUser(){
+    player1Name = document.getElementById("player1-name-input").value
+    player2Name = document.getElementById("player2-name-input").value
+    localStorage.setItem("player1Name", player1Name);
+    localStorage.setItem("player2Name", player2Name);
+    window.location = "game-page.html";
+}
+
